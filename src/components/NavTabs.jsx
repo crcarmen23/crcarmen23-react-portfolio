8c7782fbf79e89913f2f58027a4f3e4bade61acd
Carmen Regina <crcarmen23@gmail.com>
2023-11-23 00:18:21 -0600

    a few styling edits

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,13 +1,12 @@
 import { Link, useLocation } from 'react-router-dom';
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
 function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
     <div>
       <h1 className="header">Carmen Regina</h1>
+      <br />
       <ul className="nav nav-tabs">
         <li className="nav-item">
           <Link
@@ -20,7 +19,7 @@ function NavTabs() {
         <li className="nav-item">
           <Link
             to="/Portfolio"
-            // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            
             className={currentPage === '/Blog' ? 'nav-link active' : 'nav-link'}
           >
             Portfolio
