91c64b3bc71efd26a5236b9551cc3f8f29647957
Carmen Regina <crcarmen23@gmail.com>
2023-11-23 00:07:07 -0600

    added code to about meand styling

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -6,41 +6,44 @@ function NavTabs() {
   const currentPage = useLocation().pathname;
 
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <Link
-          to="/"
-          className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
-        >
-          About Me
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Portfolio"
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Blog' ? 'nav-link active' : 'nav-link'}
-        >
-          Portfolio
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/ContactMe"
-          className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact Me
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Resume"
-          className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </Link>
-      </li>
-    </ul>
+    <div>
+      <h1 className="header">Carmen Regina</h1>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <Link
+            to="/"
+            className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
+          >
+            About Me
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/Portfolio"
+            // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={currentPage === '/Blog' ? 'nav-link active' : 'nav-link'}
+          >
+            Portfolio
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/ContactMe"
+            className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+          >
+            Contact Me
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/Resume"
+            className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+          >
+            Resume
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 
--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,11 +2,16 @@
 
 export default function AboutMe() {
   return (
-    <div>
+    <div className="aboutMe">
+      <br />
       <h1>About Me</h1>
-      <p>
-      Hello! I'm Carmen Regina, a front-end web-developer based out of Dallas, Texas. I love combining logic and creative design to build captivating and user-friendly websites and applications. I have a passion for seamless design and enjoy solving problems in a smart and useful way. After completing my Bachelor's and Master's degrees in marketing and management I have decided to make a career change and am excited to take the leap and pivot into the technology industry. I hope to continue to learn everyday and expand my skills with the right company. 
-      </p>
+      <br />
+      <div className="container">
+      <aside><img className="headshot" src="/headshot.png" alt="Carmen Regina Headshot" /></aside>
+      <article><p>
+        Hello! I'm Carmen Regina, a full-stack web-developer based out of Dallas, Texas. I love combining logic and creative design to build captivating and user-friendly websites and applications, as I have a passion for seamless design and enjoy solving problems in a smart and useful way. After completing my Bachelor's and Master's degrees in marketing and management, I have decided to make a career change and am excited to take the leap and pivot into the technology industry. I hope to continue to learn everyday and expand my skills with the right company.
+      </p></article>
+      </div>
     </div>
   );
 }
