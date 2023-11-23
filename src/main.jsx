fcca187bef9784cf7cd31af7e1a01c1971626352
Carmen Regina <crcarmen23@gmail.com>
2023-11-22 19:52:20 -0600

    added code to about.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,43 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-// Bringing in the pages the router will use to conditionally show the appropriate views
 import App from './App';
-import ErrorPage from './pages/ErrorPage';
-import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
-import AboutPage from './pages/AboutPage';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Blog from './pages/Blog';
 
 // Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <ErrorPage />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
-        element: <HomePage />,
+        element: <Home />,
       },
       {
-        path: 'profile/:id',
-        element: <ProfilePage />,
+        path: '/About',
+        element: <About />,
       },
       {
-        path: 'about',
-        element: <AboutPage />,
+        path: '/Blog',
+        element: <Blog />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
       },
     ],
   },
 ]);
 
-// Render the RouterProvider component
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 );
