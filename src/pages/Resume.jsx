5f756bd9486feddcf965fe3a9b6a96ae4e16b3df
Carmen Regina <crcarmen23@gmail.com>
2023-11-22 22:18:17 -0600

    added resume pdf and proficiencies

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,35 @@
 export default function Resume() {
   return (
-    <div>
+    <div className="resume">
+      <br />
       <h1>Resume</h1>
-      <p>
-        
-      </p>
+      <br />
+      <a href="https://publuu.com/flip-book/311328/719974" className="resumeLink">Link to Resume PDF</a>
+      <br />
+      <br />
+      <div>
+        <h3>Front-end Proficiencies:</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>Responsive Design</li>
+          <li>React</li>
+          <li>Bulma</li>
+          <li>Bootstrap</li>
+        </ul>
+        <h3>Back-end Proficiencies:</h3>
+        <ul>
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MYSQL, Sequelize</li>
+          <li>MongoDB, Mongoose</li>
+          <li>REST</li>
+          <li>GraphQL</li>
+        </ul>
+      </div>
     </div>
   );
 }
