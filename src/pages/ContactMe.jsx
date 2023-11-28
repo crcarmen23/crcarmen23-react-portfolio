4961397b603f6004346d25bb76291d01f3c4f83d
Carmen Regina <crcarmen23@gmail.com>
2023-11-27 22:02:58 -0600

    got contact form working

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -3,41 +3,35 @@ import { useState } from 'react';
 
 
 function ContactMe() {
-  
+
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
-  const validateEmail=(email) => {
+  const validateEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
 
   const handleInputChange = (e) => {
- 
-    const { name, value } = e.target;
 
-    
-    switch (name) {
-      case firstName:
-        setFirstName(value)
-        break;
-      case lastName:
-        setLastName(value)
-        break;
-      case email:
-        setEmail(value)
-        break;
-      case message:
-        setMessage (value)
-        break;
-    
-      default:
-        break;
+    const { target } = e;
+    const type = target.name;
+    const value = target.value;
+
+    if (type === 'firstName') {
+      setFirstName(value)
+    } else if (type === 'lastName') {
+      setLastName(value)
+    } else if (type === 'email') {
+      setEmail(value)
+    } else {
+      setMessage(value)
     }
-    return 
+
+    return
   };
 
   const handleFormSubmit = (e) => {
@@ -46,9 +40,9 @@ function ContactMe() {
 
     if (!validateEmail(email)) {
       setErrorMessage('Email address is invalid');
-    
+
       return;
-      
+
     }
 
     // Alert the user their first and last name, clear the inputs
@@ -61,52 +55,52 @@ function ContactMe() {
     <div>
       <h1>Let's Get in Touch!</h1>
       <div className="container text-center">
-      <h1>
-       
-      </h1>
-      <form className="form" onSubmit={handleFormSubmit}>
-        <input
-          value={firstName}
-          name="firstName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="First Name"
-          required
-        />
-        <input
-          value={lastName}
-          name="lastName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Last Name"
-          required
-        />
-         <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Email Address"
-          required
-        />
-         <input
-          value={message}
-          name="message"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Message"
-          required
-        />
-        <button type="submit">
-          Submit
-        </button>
-      </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
-    </div>
+        <h1>
+
+        </h1>
+        <form className="form" onSubmit={handleFormSubmit}>
+          <input
+            value={firstName}
+            name="firstName"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="First Name"
+            required
+          />
+          <input
+            value={lastName}
+            name="lastName"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Last Name"
+            required
+          />
+          <input
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Email Address"
+            required
+          />
+          <input
+            value={message}
+            name="message"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Message"
+            required
+          />
+          <button type="submit">
+            Submit
+          </button>
+        </form>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
