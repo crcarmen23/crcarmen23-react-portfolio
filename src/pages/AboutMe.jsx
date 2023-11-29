77d1b3ddbcb78240d8bf827c41033f12ae76b515
Carmen Regina <crcarmen23@gmail.com>
2023-11-29 11:42:30 -0600

    add about me code

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,14 +4,28 @@ export default function AboutMe() {
   return (
     <div className="aboutMe min-vh-100">
       <br />
-      <h1 className="aboutHeader">About Me</h1>
+      <h2 className="aboutHeader">Hello, I'm Carmen Regina, a full-stack web developer based in Dallas, Texas.</h2>
       <br />
       <div className="container">
-      <aside><img className="headshot" src="/headshot.jpg" alt="Carmen Regina Headshot" /></aside>
-      <article className="lh-lg"><p>
-        Hello! I'm Carmen Regina, a full-stack web-developer based out of Dallas, Texas. I love combining logic and creative design to build captivating and user-friendly websites and applications, as I have a passion for seamless design and enjoy solving problems in a smart and useful way. After completing my Bachelor's and Master's degrees in marketing and management, I have decided to make a career change and am excited to take the leap and pivot into the technology industry. I hope to continue to learn everyday and expand my skills with the right company.
-      </p></article>
+        <aside><img className="headshot" src="/headshot.jpg" alt="Carmen Regina Headshot" /></aside>
+        <article className="p-5"><p>
+          My journey in technology is driven by a fusion of logic and creative design, resulting in captivating and user-friendly websites and applications. I thrive on the challenge of crafting seamless designs and solving problems in smart, practical ways.
+          <br />
+          <br />
+          My educational background includes both Bachelor's and Master's degrees in marketing and management. However, fueled by a desire for a career that merges innovation and creativity, I've decided to make the leap into the technology industry. This decision stems from my passion for staying at the forefront of technological advancements and my eagerness to contribute meaningfully to this dynamic field.
+          <br />
+          <br />
+          As a full-stack web developer, I bring a diverse set of skills to the table. From front-end interfaces that engage users to back-end functionality that ensures smooth operations, I take pride in delivering well-rounded solutions that leave a lasting impression.
+          <br />
+          <br />
+          I approach each project with enthusiasm and a commitment to continuous learning. The tech industry is ever-evolving, and I embrace the opportunity to learn something new every day. This passion for growth aligns with my aspiration to collaborate with a forward-thinking company, where I can contribute and expand my skills alongside like-minded professionals.
+          <br />
+          <br />
+          Thank you for visiting my portfolio and taking the time to learn a bit about me. I'm excited about the possibilities that lie ahead, and I invite you to explore my projects and get in touch. Let's connect and create something amazing together!
+        </p></article>
       </div>
+      <br />
+      <br />
     </div>
   );
 }
