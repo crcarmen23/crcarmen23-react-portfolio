8ddc137956c9b69aa43367ec58b5b1831f03ba43
Carmen Regina <crcarmen23@gmail.com>
2023-12-01 16:34:06 -0600

    added resume code

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -7,28 +7,32 @@ export default function Resume() {
       <a href="https://publuu.com/flip-book/311328/719974" className="resumeLink">Link to Resume PDF</a>
       <br />
       <br />
-      <div>
-        <h3>Front-end Proficiencies:</h3>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>jQuery</li>
-          <li>Responsive Design</li>
-          <li>React</li>
-          <li>Bulma</li>
-          <li>Bootstrap</li>
-        </ul>
-        <h3>Back-end Proficiencies:</h3>
-        <ul>
-          <li>APIs</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>MYSQL, Sequelize</li>
-          <li>MongoDB, Mongoose</li>
-          <li>REST</li>
-          <li>GraphQL</li>
-        </ul>
+      <div className="d-inline-flex ">
+        <div className="card .g col-md-6 col-sm-12 frontEndProf m-3">
+          <h3 className="card-title">Front-end Proficiencies:</h3>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">HTML</li>
+            <li className="list-group-item">CSS</li>
+            <li className="list-group-item">JavaScript</li>
+            <li className="list-group-item">jQuery</li>
+            <li className="list-group-item">Responsive Design</li>
+            <li className="list-group-item">React</li>
+            <li className="list-group-item">Bulma</li>
+            <li className="list-group-item">Bootstrap</li>
+          </ul>
+        </div>
+        <div className="card .g col-md-6 col-sm-12 backEndProf m-3">
+          <h3 className="card-title">Back-end Proficiencies:</h3>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">APIs</li>
+            <li className="list-group-item">Node</li>
+            <li className="list-group-item">Express</li>
+            <li className="list-group-item">MYSQL, Sequelize</li>
+            <li className="list-group-item">MongoDB, Mongoose</li>
+            <li className="list-group-item">REST</li>
+            <li className="list-group-item">GraphQL</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
